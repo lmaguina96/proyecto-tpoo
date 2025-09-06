60f74cf621846f548ad7377e2183b784f0900d93
ĺaura <jhamely.19@hotmail.com>
2025-09-06 00:47:57 -0500

    usuariosimple:cambio de texto

--- caso1/UsuarioSimple.java
+++ caso1/UsuarioSimple.java
@@ -7,7 +7,7 @@ public class UsuarioSimple {
 		
 		Scanner scanner = new Scanner(System.in);
 	
-        System.out.println("Por favor, ingresa tu nombre:");
+        System.out.println("Por favor, ingresa solo tu primer nombre :");
         String nombre = scanner.nextLine();
 
         System.out.println("Ahora, ingresa tu edad:");
